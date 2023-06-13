a7d9dff2e2a85c58fc84636ce0c12c5b776fa6be
Corey Wright <corey.wright@sky.uk>
2023-06-13 16:58:43 +0100

    Functional_tests Co-Authored-by: <Anthony.Ingegnere@sky.uk> Anthony Ingegnere

--- functional/src/test/java/com/sky/nebula/carDealership/functional/stepdefinitions/PrivateStepDefinitions.java
+++ functional/src/test/java/com/sky/nebula/carDealership/functional/stepdefinitions/PrivateStepDefinitions.java
@@ -1,18 +1,20 @@
 package com.sky.nebula.carDealership.functional.stepdefinitions;
 
-import com.sky.nebula.carDealership.model.Car;
-import io.cucumber.cienvironment.internal.com.eclipsesource.json.JsonObject;
-import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sky.nebula.carDealership.functional.config.datatabletype.Car;
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapper;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -24,6 +26,19 @@ public class PrivateStepDefinitions {
 
     String json;
 
+    private RequestSpecification request;
+
+    @DataTableType
+    public Car carEntry(Map<String, String> entry) {
+        return new Car(
+                entry.get("brand"),
+                entry.get("model"),
+                Integer.parseInt(entry.get("year")),
+                Integer.parseInt(entry.get("price")),
+                Integer.parseInt(entry.get("mileage")),
+                entry.get("colour")
+        );
+    }
 
     @When("client sends a {string} request to {string} endpoint")
     public void clientSendsARequestToEndpoint(String requestType, String endpoint) {
@@ -48,6 +63,20 @@ public class PrivateStepDefinitions {
     public void theResponseBodyShouldContainOK(String bodyMessage) {
         json = response.asString();
         Assertions.assertEquals(bodyMessage, json);
+    }
+
+
+
+
+    @And("the response body should have the key {string} with the value of {string}")
+    public void theResponseBodyShouldHaveTheKeyDatabaseWithTheValueOfUpdated() {
+    }
+
+    @Given("the body of the car model is")
+    public RequestSpecification theBodyOfTheCarModelIs(List<Car> cars) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(cars);
+        return given().contentType(ContentType.JSON).body(json);
     }
 
 }
