d4e77185b488bac93469a199771a3ee1c2099f7e
Anthony Ingegnere  & Corey Wright <anthony.ingegnere@sky.uk + corey.wright@sky.uk>
2023-07-11 10:13:29 +0100

    NEB-348 Exception-Handling: Unit tests completed for GlobalExceptionHandler

--- car-dealership-service/src/test/java/com/sky/nebula/carDealership/exceptionHandlers/ExceptionHandlerTests.java
+++ car-dealership-service/src/test/java/com/sky/nebula/carDealership/exceptionHandlers/ExceptionHandlerTests.java
@@ -1,15 +1,16 @@
 package com.sky.nebula.carDealership.exceptionHandlers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sky.nebula.carDealership.controllers.CarController;
-
 import com.sky.nebula.carDealership.exceptions.CarAlreadyExistsException;
 import com.sky.nebula.carDealership.exceptions.InvalidDataException;
 import com.sky.nebula.carDealership.globalExceptionHandler.GlobalExceptionHandler;
 import com.sky.nebula.carDealership.model.Car;
 import com.sky.nebula.carDealership.repository.CarRepository;
 import com.sky.nebula.carDealership.service.CarService;
-import io.cucumber.java.it.Ma;
+import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.core.internal.com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,14 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.mock.web.reactive.function.server.MockServerRequest;
 
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import java.util.Collections;
+import java.util.Map;
 
 public class ExceptionHandlerTests {
 
@@ -44,11 +43,12 @@ public class ExceptionHandlerTests {
         carController = new CarController(carService);
     }
 
-    @Test
-    void handleCarAlreadyExistsExceptionTest() {
 
+    @Test
+    void handleCarAlreadyExistsExceptionTest() throws CarAlreadyExistsException {
+
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler.handleCarAlreadyExists();
         Car car = new Car("BMW", "X5", 2022, 10000, 100000, "space grey");
-        ResponseEntity<Map<String, String>> response = carController.addCar(Collections.singletonList(car));
         Mockito.when(carRepository.existsByBrandAndModelAndYearAndPriceAndMileageAndColour(
                 car.getBrand(),
                 car.getModel(),
@@ -58,37 +58,79 @@ public class ExceptionHandlerTests {
                 car.getColour()
         )).thenReturn(true);
 
+        HttpStatus expectedStatus = HttpStatus.CONFLICT;
         String key = "Description";
         String value = "Car already exists";
 
-        Assertions.assertTrue(response.getBody().containsKey(key));
-        Assertions.assertFalse(response.getBody().containsValue(value));
-
-        assertThrows(CarAlreadyExistsException.class, () ->
+        Assertions.assertThrows(CarAlreadyExistsException.class, () ->
                 carController.addCar(Collections.singletonList(car)));
+
+        Assertions.assertEquals(expectedStatus, response.getStatusCode());
+        Assertions.assertTrue(response.getBody().containsKey(key));
+        Assertions.assertTrue(response.getBody().containsValue(value));
     }
 
 
     @Test
-    void handleValidInputExceptionTest(){
+    void addCarInvalidFieldDataReturns400AndErrorMessage() throws InvalidDataException {
 
         ResponseEntity<Map<String, String>> response = globalExceptionHandler.handleInvalidInput();
+
+        Car testCarMissingData = new Car("", "X5", null, 10000, 100000, "space grey");
 
         HttpStatus expectedStatus = HttpStatus.BAD_REQUEST;
         String key = "Description";
         String value = "Incorrect car data provided";
 
-        Assertions.assertEquals(expectedStatus, response.getStatusCode());
+        Assertions.assertThrows(InvalidDataException.class, () ->
+                carController.addCar(Collections.singletonList(testCarMissingData)));
 
+        Assertions.assertEquals(expectedStatus, response.getStatusCode());
         Assertions.assertTrue(response.getBody().containsKey(key));
         Assertions.assertTrue(response.getBody().containsValue(value));
     }
 
-    @Test()
-    void addCarMissingFieldDataReturns400AndErrorMessage() throws InvalidDataException {
-        Car testCarMissingData = new Car("", "X5", null, 10000, 100000, "space grey");
-        Assertions.assertThrows(InvalidDataException.class, () ->
-                carController.addCar(Collections.singletonList(testCarMissingData)));
-    }
+//    @Test
+//    void addCarInvalidAttributeDataReturns400AndErrorMessage() throws InvalidDataException {
+//
+//        String requestBody = "[{\"\":\"BMW\",\"model\":\"X6\",\"year\":2023,\"price\":18800,\"mileage\":10000,\"colour\":\"space grey\"}]";
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+//
+//        MockServerRequest.Builder restTemplate = null;
+//        ResponseEntity<Map<String, String>> response = restTemplate.exchange("/cars/admin", HttpMethod.POST, request, new ParameterizedTypeReference<Map<String, String>>() {});
+//
+//        HttpStatus expectedStatus = HttpStatus.BAD_REQUEST;
+//        String key = "Description";
+//        String value = "Incorrect car data provided";
+//
+//        Assertions.assertEquals(expectedStatus, response.getStatusCode());
+//        Assertions.assertTrue(response.getBody().containsKey(key));
+//        Assertions.assertTrue(response.getBody().containsValue(value));
+//    }
+//
+//    @Test
+//    void testInvalidAttributeExceptionHandling() throws InvalidDataException {
+//        // Create a malformed car object
+//        String malformedCarJson = "{ \"bra\": \"BMW\", \"model\": \"A5\", \"year\": 1900, \"price\": 10000, \"mileage\": 663000, \"colour\": \"sky blue\" }";
+//        // Use the Jackson library to map the JSON to a Map object
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Map<String, Object> malformedCarData;
+//
+//        // Mock the repository method call
+//        Mockito.when(carRepository.existsByBrandAndModelAndYearAndPriceAndMileageAndColour(
+//                (String) malformedCarData.get("bra"),
+//                (String) malformedCarData.get("model"),
+//                (Integer) malformedCarData.get("year"),
+//                (Integer) malformedCarData.get("price"),
+//                (Integer) malformedCarData.get("mileage"),
+//                (String) malformedCarData.get("colour")
+//        )).thenReturn(true);
+//        // We expect the method call to throw an InvalidAtributeException
+//        Assertions.assertThrows(InvalidDataException.class, () -> carController.addCar(Collections.singletonList(malformedCarData)));
+//    }
 
 }
